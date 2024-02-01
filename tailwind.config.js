6e333064ad3bff731e489bcc38ab7322568b0279
Jeff Griffiths <jeff@eko-recordings.ca>
2024-02-01 13:31:04 -0800

    first cut of the static site

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,23 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("daisyui"), 
+    require('@tailwindcss/typography')
+  ],
+  daisyui: {
+    themes: [{
+      synthwave: { 
+        "primary": "#ff00ff",
+        "secondary": "#ffffff",
+        "accent": "#ffffff",
+        "neutral": "#ffffff",
+        "base-100": "#ffffff",
+        "info": "#ffffff",
+        "success": "#00ffff",
+        "warning": "#ffffff",
+        "error": "#ffffff"
+      },
+    }],    
+  }
 }
