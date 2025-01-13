b480263990026ec2b5b8d34b6e5441a50af6b7b5
Jeff Griffiths <jeff@eko-recordings.ca>
2025-01-13 10:41:38 -0800

    fixed ts build

--- src/server.ts
+++ src/server.ts
@@ -10,60 +10,62 @@ import { FirehoseSubscription } from './subscription'
 import { AppContext, Config } from './config'
 import wellKnown from './well-known'
 
-export class FeedGenerator {
-  public app: express.Application
-  public server?: http.Server
-  public db: Database
-  public firehose: FirehoseSubscription
-  public cfg: Config
+const FeedGenerator = {}
 
-  constructor(
-    app: express.Application,
-    db: Database,
-    firehose: FirehoseSubscription,
-    cfg: Config,
-  ) {
-    this.app = app
-    this.db = db
-    this.firehose = firehose
-    this.cfg = cfg
-  }
+// export class FeedGenerator {
+//   public app: express.Application
+//   public server?: http.Server
+//   public db: Database
+//   public firehose: FirehoseSubscription
+//   public cfg: Config
 
-  static create(cfg: Config) {
-    const app = express()
+//   constructor(
+//     app: express.Application,
+//     db: Database,
+//     firehose: FirehoseSubscription,
+//     cfg: Config,
+//   ) {
+//     this.app = app
+//     this.db = db
+//     this.firehose = firehose
+//     this.cfg = cfg
+//   }
 
-    app.use((req, res, next) => {
-      res.send({
-        cursor:
-          '1736788677461::bafyreiardf5bo26qxtq7fnzpc557dvaxyja3yfsmnw7qkqnzwjg7cycxym',
-        feed: [
-          {
-            post: 'at://did:plc:r33d6wynfbi5mfinrjqfbmt5/app.bsky.feed.post/3lfnd4dkpi22t',
-          },
-        ],
-      })
-    })
+//   static create(cfg: Config) {
+//     const app = express()
 
-    const server = createServer({
-      validateResponse: true,
-      payload: {
-        jsonLimit: 100 * 1024, // 100kb
-        textLimit: 100 * 1024, // 100kb
-        blobLimit: 5 * 1024 * 1024, // 5mb
-      },
-    })
+//     app.use((req, res, next) => {
+//       res.send({
+//         cursor:
+//           '1736788677461::bafyreiardf5bo26qxtq7fnzpc557dvaxyja3yfsmnw7qkqnzwjg7cycxym',
+//         feed: [
+//           {
+//             post: 'at://did:plc:r33d6wynfbi5mfinrjqfbmt5/app.bsky.feed.post/3lfnd4dkpi22t',
+//           },
+//         ],
+//       })
+//     })
 
-    server.use(app)
-    return server
-  }
+//     const server = createServer({
+//       validateResponse: true,
+//       payload: {
+//         jsonLimit: 100 * 1024, // 100kb
+//         textLimit: 100 * 1024, // 100kb
+//         blobLimit: 5 * 1024 * 1024, // 5mb
+//       },
+//     })
 
-  async start(): Promise<http.Server> {
-    await migrateToLatest(this.db)
-    this.firehose.run(this.cfg.subscriptionReconnectDelay)
-    this.server = this.app.listen(this.cfg.port, this.cfg.listenhost)
-    await events.once(this.server, 'listening')
-    return this.server
-  }
-}
+//     server.use(app)
+//     return server
+//   }
+
+//   async start(): Promise<http.Server> {
+//     await migrateToLatest(this.db)
+//     this.firehose.run(this.cfg.subscriptionReconnectDelay)
+//     this.server = this.app.listen(this.cfg.port, this.cfg.listenhost)
+//     await events.once(this.server, 'listening')
+//     return this.server
+//   }
+// }
 
 export default FeedGenerator
